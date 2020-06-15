d1328729b652cdab5d29553b8888e1fcd26d5cf2
Gianluca1993 <gianluca.tiribelli@gmail.com>
2020-06-15 17:02:04 +0200

    aggiunto TestUtenteDao, corretto UtenteDao

---
+++ src/test/java/testDao/TestUtenteDao.java
@@ -0,0 +1,49 @@
+package testDao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import com.stoteam.attori.Utente;
+import com.stoteam.dao.DbConnection;
+import com.stoteam.dao.UtenteDao;
+
+class TestUtenteDao {
+
+	@Test
+	void test_getName() {
+        // given
+        String name = "John";
+        String surname = "Doe";
+    	String telefono = "000000111";
+    	String email = "john@doea.it";
+    	String password = "aaablablabla";
+    	String indirizzo = "via le mani dal culo, 69";
+    	String codiceFiscale = "BBBZZZ00B00H000L";
+    	int tipoUtente = 1;
+        Utente account = new Utente(name, surname, telefono, email, password, tipoUtente, indirizzo, codiceFiscale);
+        
+        // then
+        Connection c = DbConnection.Connect();
+        UtenteDao.UpUtente(c, account);
+        Utente accountDb = UtenteDao.getUtente(c, account.getId());
+        System.out.println(accountDb.toString());
+        String result = accountDb.getNome();
+        UtenteDao.removeUtente(c, accountDb.getId());
+        //System.out.println(result);
+        try {
+			c.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        // expect
+        assertEquals(name, result, "The name must be the same");
+    }
+
+}
