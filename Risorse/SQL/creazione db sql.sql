c4755bebe6c0bb3a00c254aae5a660463db78475
Gianluca1993 <gianluca.tiribelli@gmail.com>
2020-06-24 21:52:09 +0200

    aggiornato sql e modello

--- Risorse/SQL/creazione db sql.sql
+++ Risorse/SQL/creazione db sql.sql
@@ -1,102 +1,212 @@
-CREATE TABLE intestatario (
-	id int auto_increment primary key
-);
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE utente (
-	id int auto_increment primary key ,
-	nome varchar(255),
-    cognome varchar(255),
-    telefono char(11),
-    email varchar(255) unique,
-	pass varchar(1024),
-    tipo_utente int,
-    indirizzo varchar(255),
-    codice_fiscale char(16),
-    id_intestatario int,
-    FOREIGN KEY (id_intestatario) REFERENCES intestatario(id)
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE TABLE amministratore (
-	id int auto_increment primary key,
-	nome varchar(255),
-    cognome varchar(255),
-    telefono char(11),
-    email varchar(255) unique,
-    pass varchar(1024),
-    tipo_utente varchar(20),
-    indirizzo varchar(255),
-    livello_accesso varchar(20),
-    area_competenza varchar(255)
-);
+-- -----------------------------------------------------
+-- Schema stabanca!
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `stabanca!` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `stabanca!` ;
 
-CREATE TABLE aziende (
-	id int auto_increment primary key ,
-	nome varchar(255),
-    cognome varchar(255),
-    telefono char(11),
-    email varchar(255) unique,
-	passw varchar(1024),
-    tipo_utente int,
-    indirizzo varchar(255),
-    ragione_sociale varchar(255),
-    partita_iva char(22) unique,
-    id_intestatario int,
-    FOREIGN KEY (id_intestatario) REFERENCES intestatario(id)
-);
+-- -----------------------------------------------------
+-- Table `stabanca!`.`amministratore`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stabanca!`.`amministratore` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL DEFAULT NULL,
+  `cognome` VARCHAR(255) NULL DEFAULT NULL,
+  `telefono` CHAR(11) NULL DEFAULT NULL,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `pass` VARCHAR(1024) NULL DEFAULT NULL,
+  `tipo_utente` VARCHAR(20) NULL DEFAULT NULL,
+  `indirizzo` VARCHAR(255) NULL DEFAULT NULL,
+  `livello_accesso` VARCHAR(20) NULL DEFAULT NULL,
+  `area_competenza` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email` (`email` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE conto (
-	id int auto_increment primary key,
-    codice_conto int unique,
-    iban char(27) unique,
-    saldo double,
-    saldo_contabile double,
-    id_intestatario int,
-    FOREIGN KEY (id_intestatario) REFERENCES intestatario(id)
-);
 
-CREATE TABLE carta (
-	id int auto_increment primary key,
-	is_block boolean,
-    spesa_mensile double,
-    data_rilascio date,
-    data_scadenza date,
-    codice_sicurezza char(3),
-    pin char(5),
-    limite double,
-    disponibilita double,
-    uso_pin boolean,
-    conto_id int,
-    FOREIGN KEY (conto_id) REFERENCES conto(id)
-);
+-- -----------------------------------------------------
+-- Table `stabanca!`.`intestatario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stabanca!`.`intestatario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 189
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-CREATE TABLE movimento_conto (
-	id int auto_increment primary key,
-    tipo_movimento varchar(255),
-    importo double,
-    conto_id_m int,                       -- CodiceConto Mittente
-    data_esecuzione Date,
-    conto_id_d int,                       -- CodiceConto Destinatario
-    data_arrivo Date,
-    causale varchar(1024),
-    carta_id int,
-    FOREIGN KEY (conto_id_m) REFERENCES conto(id),
-    FOREIGN KEY (conto_id_d) REFERENCES conto(id),
-    FOREIGN KEY (carta_id) REFERENCES carta(id)
-);
 
-CREATE TABLE prestito (
-	id int auto_increment primary key,
-    importo double,
-	dovuti double,
-	pagati double,
-	tan double,
-	taeg double,
-	tempo int,                           -- per quanto tempo dura il prestito
-	is_fisso boolean,
-	is_approvato boolean,
-	data_inizio date,
-	data_fine date,
-	data_rata date,
-	conto_id int,
-    FOREIGN KEY (conto_id) REFERENCES conto(id)
-);
+-- -----------------------------------------------------
+-- Table `stabanca!`.`aziende`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stabanca!`.`aziende` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL DEFAULT NULL,
+  `cognome` VARCHAR(255) NULL DEFAULT NULL,
+  `telefono` CHAR(11) NULL DEFAULT NULL,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `passw` VARCHAR(1024) NULL DEFAULT NULL,
+  `tipo_utente` INT NULL DEFAULT NULL,
+  `indirizzo` VARCHAR(255) NULL DEFAULT NULL,
+  `ragione_sociale` VARCHAR(255) NULL DEFAULT NULL,
+  `partita_iva` CHAR(22) NULL DEFAULT NULL,
+  `id_intestatario` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `partita_iva` (`partita_iva` ASC) VISIBLE,
+  INDEX `id_intestatario` (`id_intestatario` ASC) VISIBLE,
+  CONSTRAINT `aziende_ibfk_1`
+    FOREIGN KEY (`id_intestatario`)
+    REFERENCES `stabanca!`.`intestatario` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 31
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `stabanca!`.`conto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stabanca!`.`conto` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `codice_conto` VARCHAR(255) NULL DEFAULT NULL,
+  `iban` CHAR(27) NULL DEFAULT NULL,
+  `saldo` DOUBLE NULL DEFAULT NULL,
+  `saldo_contabile` DOUBLE NULL DEFAULT NULL,
+  `id_intestatario` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `codice_conto` (`codice_conto` ASC) VISIBLE,
+  UNIQUE INDEX `iban` (`iban` ASC) VISIBLE,
+  INDEX `id_intestatario` (`id_intestatario` ASC) VISIBLE,
+  CONSTRAINT `conto_ibfk_1`
+    FOREIGN KEY (`id_intestatario`)
+    REFERENCES `stabanca!`.`intestatario` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 96
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `stabanca!`.`carta`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stabanca!`.`carta` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `is_block` TINYINT(1) NULL DEFAULT NULL,
+  `spesa_mensile` DOUBLE NULL DEFAULT NULL,
+  `data_rilascio` DATETIME NULL DEFAULT NULL,
+  `data_scadenza` DATETIME NULL DEFAULT NULL,
+  `codice_sicurezza` CHAR(3) NULL DEFAULT NULL,
+  `pin` CHAR(5) NULL DEFAULT NULL,
+  `limite` DOUBLE NULL DEFAULT NULL,
+  `disponibilita` DOUBLE NULL DEFAULT NULL,
+  `uso_pin` TINYINT(1) NULL DEFAULT NULL,
+  `conto_id` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `conto_id` (`conto_id` ASC) VISIBLE,
+  CONSTRAINT `carta_ibfk_1`
+    FOREIGN KEY (`conto_id`)
+    REFERENCES `stabanca!`.`conto` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 12
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `stabanca!`.`movimento_conto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stabanca!`.`movimento_conto` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `tipo_movimento` VARCHAR(255) NULL DEFAULT NULL,
+  `importo` DOUBLE NULL DEFAULT NULL,
+  `conto_iban_m` CHAR(27) NULL DEFAULT NULL,
+  `data_esecuzione` DATETIME NULL DEFAULT NULL,
+  `is_eseguito` TINYINT NULL DEFAULT NULL,
+  `conto_iban_d` CHAR(27) NULL DEFAULT NULL,
+  `data_arrivo` DATETIME NULL DEFAULT NULL,
+  `causale` VARCHAR(1024) NULL DEFAULT NULL,
+  `carta_id` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `carta_id` (`carta_id` ASC) VISIBLE,
+  INDEX `movimento_conto_ibfk_1` (`conto_iban_m` ASC) VISIBLE,
+  INDEX `movimento_conto_ibfk_2` (`conto_iban_d` ASC) VISIBLE,
+  CONSTRAINT `movimento_conto_ibfk_1`
+    FOREIGN KEY (`conto_iban_m`)
+    REFERENCES `stabanca!`.`conto` (`iban`),
+  CONSTRAINT `movimento_conto_ibfk_2`
+    FOREIGN KEY (`conto_iban_d`)
+    REFERENCES `stabanca!`.`conto` (`iban`),
+  CONSTRAINT `movimento_conto_ibfk_3`
+    FOREIGN KEY (`carta_id`)
+    REFERENCES `stabanca!`.`carta` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 37
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `stabanca!`.`prestito`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stabanca!`.`prestito` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `importo` DOUBLE NULL DEFAULT NULL,
+  `dovuti` DOUBLE NULL DEFAULT NULL,
+  `pagati` DOUBLE NULL DEFAULT NULL,
+  `tan` DOUBLE NULL DEFAULT NULL,
+  `taeg` DOUBLE NULL DEFAULT NULL,
+  `tempo` INT NULL DEFAULT NULL,
+  `is_fisso` TINYINT(1) NULL DEFAULT NULL,
+  `is_approvato` TINYINT(1) NULL DEFAULT NULL,
+  `data_inizio` DATE NULL DEFAULT NULL,
+  `data_fine` DATE NULL DEFAULT NULL,
+  `data_rata` DATE NULL DEFAULT NULL,
+  `conto_id` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `conto_id` (`conto_id` ASC) VISIBLE,
+  CONSTRAINT `prestito_ibfk_1`
+    FOREIGN KEY (`conto_id`)
+    REFERENCES `stabanca!`.`conto` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `stabanca!`.`utente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `stabanca!`.`utente` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NULL DEFAULT NULL,
+  `cognome` VARCHAR(255) NULL DEFAULT NULL,
+  `telefono` CHAR(11) NULL DEFAULT NULL,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `pass` VARCHAR(1024) NULL DEFAULT NULL,
+  `tipo_utente` VARCHAR(20) NULL DEFAULT NULL,
+  `indirizzo` VARCHAR(255) NULL DEFAULT NULL,
+  `codice_fiscale` CHAR(16) NULL DEFAULT NULL,
+  `id_intestatario` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `codice_fiscale` (`codice_fiscale` ASC) VISIBLE,
+  INDEX `id_intestatario` (`id_intestatario` ASC) VISIBLE,
+  CONSTRAINT `utente_ibfk_1`
+    FOREIGN KEY (`id_intestatario`)
+    REFERENCES `stabanca!`.`intestatario` (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 159
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
